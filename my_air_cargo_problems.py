91d4b70f3a02802fb81604a863fa0458b343411d
Austin Totty <awtotty@gmail.com>
2017-07-05 19:53:04 +0000

    Implement result

--- my_air_cargo_problems.py
+++ my_air_cargo_problems.py
@@ -140,6 +140,7 @@ class AirCargoProblem(Problem):
         :return: list of Action objects
         """
         # TODO implement
+        # TODO Comment
         possible_actions = []
         kb = PropKB()
         kb.tell(decode_state(state, self.state_map).pos_sentence())
@@ -159,7 +160,22 @@ class AirCargoProblem(Problem):
         :return: resulting state after action
         """
         # TODO implement
+        # TODO Comment
         new_state = FluentState([], [])
+        old_state = decode_state(state, self.state_map)
+
+        for fluent in old_state.pos:
+            if fluent not in action.effect_rem:
+                new_state.pos.append(fluent)
+        for fluent in action.effect_add:
+            if fluent not in new_state.pos:
+                new_state.pos.append(fluent)
+        for fluent in old_state.neg:
+            if fluent not in action.effect_add:
+                new_state.neg.append(fluent)
+        for fluent in action.effect_rem:
+            if fluent not in new_state.neg:
+                new_state.neg.append(fluent)
         return encode_state(new_state, self.state_map)
 
     def goal_test(self, state: str) -> bool:
